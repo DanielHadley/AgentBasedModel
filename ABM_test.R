4dcafcc507f7c219a733c4afe7758f5a3e8adc3d
Daniel Hadley <dphnrome@gmail.com>
2016-08-08 14:41:59 -0600

    local data in before starting to put functions out of the main model

--- ABM_test.R
+++ ABM_test.R
@@ -23,7 +23,8 @@ survival_rates <- read.csv("./clean_data/mschpprts05f02.csv",
 # I pieced this data together using an email from Julie Christensen, which contained recidivism rates for the COD demographic, and the Justice Reinvestment Report, which contained the average sentencing:
 # http://justice.utah.gov/Documents/CCJJ/Reports/Justice_Reinvestment_Report_2014.pdf
 # Before getting to the crime types and average sentences, though, it's important to grasp this statistic: 70% of parolees are returned on a technical violation. Only 30% are returned for crimes. WOW. Of those 70%, I estimate that 37 of the 70% are for parole violations, and 33 of the 70 are for probation violations
-utah_cod_recidivism_rates <- read.csv("./clean_data/utah_cod_recidivism_rates.csv")
+prison_terms <- read.csv("./clean_data/utah_cod_recidivism_rates.csv") %>% 
+  mutate(mean_time_served = round(mean_time_served))
 
 
 
@@ -70,10 +71,10 @@ agent <- function(months) {
     # Also based on probability table from national trends 
     m.prison_sentence <- if_else(m.rearrested_or_not == 1,
                                  as.numeric(
-                                   sample(x = as.vector(prison_terms$median_term),
+                                   sample(x = as.vector(prison_terms$mean_time_served),
                                           size = 1,
                                           replace = FALSE,
-                                          prob = c(prison_terms$percent))
+                                          prob = c(prison_terms$frequency))
                                  ),
                                  ifelse(month == 1, 0,
                                         ifelse(m.rearrested_or_not == 0,
@@ -99,13 +100,13 @@ agent_test <- agent(60)
 
 
 # Here is a test to see if the percent of people re-arrested matches the data in the survival rates df
-count_rearrested <- 1
-
-for (i in 1:1000) {
-  basic <- 0
-  agent_test <- agent(60)
-  arrested <- (sum(agent_test$rearrested_or_not))
-  if (arrested > 0) {
-    count_rearrested <- count_rearrested + 1
-  }
-}
+# count_rearrested <- 1
+# 
+# for (i in 1:1000) {
+#   basic <- 0
+#   agent_test <- agent(60)
+#   arrested <- (sum(agent_test$rearrested_or_not))
+#   if (arrested > 0) {
+#     count_rearrested <- count_rearrested + 1
+#   }
+# }
