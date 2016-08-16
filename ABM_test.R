ade495f8e904170d599aa591d8e2fa023af2d9cd
Daniel Hadley <dphnrome@gmail.com>
2016-08-16 14:03:22 -0600

    added more total costs

--- ABM_test.R
+++ ABM_test.R
@@ -177,12 +177,33 @@ calc_total_policing_costs <- function(m.rearrested_or_not, m.crime_type){
 # Not sure how to think of this. Based on frequent users, it's possible that about 1/5 of the time these folks are out of prison, they are in a shelter. That's a guess. It would be good to replace with actual data
 # http://www.urban.org/sites/default/files/alfresco/publication-pdfs/412504-Frequent-Users-of-Jail-and-Shelter-Systems-in-the-District-of-Columbia-An-Overview-of-the-Potential-for-Supportive-Housing.PDF
 # http://www.endhomelessness.org/page/-/files/1101_file_Cho_Presentation.pdf
-define_shelter_days <- function(m.prison_sentence){
+define_shelter_days <- function(m.is_in_prison){
   
-  ifelse(m.prison_sentence > 0, 0,
+  ifelse(m.is_in_prison == 1, 0,
          sample(x = (c(1,0)), 1, prob = c(.2, .8)))
   
 }
+
+
+# There are a lot of possibilities for calculating the costs. Yvette sent data on Palmer Court and other shelters. Julia sent some data on the cost to operate and number of folks in halfway houses. There is a huge variety in costs. I think Yvette's estimate is a good place to start. 
+calc_shelter_costs <- function(m.is_in_shelter){
+  
+  ifelse(m.is_in_shelter == 1, 10.10 * 30, 0)
+  
+}
+
+
+# Adult probation and parole
+calc_total_app_costs <- function(m.is_in_prison, m.months_free){
+  
+  # Based on the cost per day data
+  # And average length of stay in parole
+  
+  ifelse(m.is_in_prison == 0 & m.months_free < 23, 10 * 30, 0)
+  
+}
+
+
 
 
 #### A function with the single-agent model : this generates data for one person ####
@@ -202,7 +223,9 @@ sim_single_agent <- function(months) {
                    total_prison_costs=numeric(0),
                    total_court_costs=numeric(0),
                    total_policing_costs=numeric(0),
-                   is_in_shelter=numeric(0))
+                   is_in_shelter=numeric(0),
+                   total_shelter_costs=numeric(0),
+                   total_probation_costs=numeric(0))
   
   # loop through each month and see what happens			
   for (month in 1:months) {
@@ -241,14 +264,19 @@ sim_single_agent <- function(months) {
     m.total_policing_costs <- calc_total_policing_costs(m.rearrested_or_not, m.crime_type)
     
     # Shelters 
-    m.is_in_shelter <- define_shelter_days(m.prison_sentence)
+    m.is_in_shelter <- define_shelter_days(m.is_in_prison)
+    m.total_shelter_costs <- calc_shelter_costs(m.is_in_shelter)
+    
+    # Probation and Parole
+    m.total_probation_costs <- calc_total_app_costs(m.is_in_prison, m.months_free)
     
     
     # add month to the data frame
     df[month,] <- 
       c(m.month, m.months_free, m.rearrested_or_not, m.prison_sentence, 
         m.crime_type, m.is_in_prison, m.marginal_prison_costs, m.total_prison_costs,
-        m.total_court_costs, m.total_policing_costs, m.is_in_shelter)
+        m.total_court_costs, m.total_policing_costs, m.is_in_shelter, 
+        m.total_shelter_costs, m.total_probation_costs)
     
     
     # Make temporary vector for determining months free in the next pass
@@ -273,7 +301,9 @@ sim_multi_agents <- function(n_agents, n_months){
   df <- data.frame(prison_time=numeric(0),
                    months_free=numeric(0),
                    prison_costs=numeric(0),
-                   arrests=numeric(0))
+                   arrests=numeric(0),
+                   total_costs=numeric(0),
+                   avg_total_cost_per_yr=numeric(0))
   
   # loop through each month and see what happens			
   for (agent in 1:n_agents) {
@@ -284,13 +314,23 @@ sim_multi_agents <- function(n_agents, n_months){
     # Sum things that happend to the agent
     a.prison_time <- sum(as.numeric(single_agent$is_in_prison))
     a.months_free <- sum(as.numeric(single_agent$months_free))
-    a.prison_costs <- sum(as.numeric(single_agent$prison_costs))
+    a.total_prison_costs <- sum(as.numeric(single_agent$total_prison_costs))
     a.arrests <- sum(as.numeric(single_agent$rearrested_or_not))
+    
+    # Get total costs
+    total_costs <- single_agent %>% 
+      select(starts_with("total"))
+    
+    total_costs <- sapply(total_costs, as.numeric)
+    a.total_costs <- sum(colSums(total_costs))
+    
+    a.avg_total_cost_per_yr <- a.total_costs / (n_months / 12)
     
     
     # add month to the data frame
     df[agent,] <- 
-      c(a.prison_time, a.months_free, a.prison_costs, a.arrests)
+      c(a.prison_time, a.months_free, a.total_prison_costs, a.arrests, a.total_costs,
+        a.avg_total_cost_per_yr)
     
   }
   
@@ -325,6 +365,15 @@ mean(multi_agent_test$prison_costs)
 hist(multi_agent_test$arrests)
 mean(multi_agent_test$arrests)
 table(multi_agent_test$arrests)
+
+# Costs
+hist(multi_agent_test$total_costs)
+mean(multi_agent_test$total_costs)
+
+hist(multi_agent_test$avg_total_cost_per_yr)
+mean(multi_agent_test$avg_total_cost_per_yr)
+summary(multi_agent_test$avg_total_cost_per_yr)
+
 
 
 
